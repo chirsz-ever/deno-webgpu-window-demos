9abac6eab5d92ab13f0f30a64cc641b750f327d7
chirsz-ever <chirsz-ever@outlook.com>
2024-04-26 20:27:18 +0800

    support to use OrbitControls with left mouse button

--- threejs/polyfill.ts
+++ threejs/polyfill.ts
@@ -12,9 +12,96 @@ type InitParams = {
     height: number,
 };
 
+class MouseEvent extends Event {
+    altKey: boolean = false;
+    button: number = 0;
+    buttons: number = 0;
+    clientX: number = 0;
+    clientY: number = 0;
+    ctrlKey: boolean = false;
+    // layerX: number = 0;
+    // layerY: number = 0;
+    metaKey: boolean = false;
+    movementX: number = 0;
+    movementY: number = 0;
+    offsetX: number = 0;
+    offsetY: number = 0;
+    pageX: number = 0;
+    pageY: number = 0;
+    relatedTarget: EventTarget | null = null;
+    screenX: number = 0;
+    screenY: number = 0;
+    shiftKey: boolean = false;
+    x: number = 0;
+    y: number = 0;
+
+    constructor(name: string, options?: EventInit) {
+        super(name, options)
+    }
+}
+
+const ignoredEvents = [
+    "pointerdown", "pointermove", "pointerup"
+];
+
+class CanvasDomMock extends EventTarget {
+    style = {}
+
+    get clientHeight() {
+        return this.height;
+    }
+
+    get clientWidth() {
+        return this.width;
+    }
+
+    constructor(private canvas: Deno.UnsafeWindowSurface, private width: number, private height: number) {
+        super();
+    }
+
+    addEventListener(event: string, listener: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions) {
+        super.addEventListener(event, listener, options);
+        if (!ignoredEvents.includes(event))
+            console.info(`canvas.addEventListener("${event}", ...)`)
+    }
+
+    getRootNode() {
+        console.info(`canvas.getRootNode()`);
+        return this;
+    }
+
+    getContext(name: string) {
+        console.info(`canvas.getContext("${name}")`);
+        return this.canvas.getContext(name as any);
+    }
+
+    setPointerCapture() {
+        // console.info(`canvas.setPointerCapture()`);
+    }
+
+    releasePointerCapture() {
+        // console.info(`canvas.releasePointerCapture()`);
+    }
+}
+
+let lastMoveMouseEvent: MouseEvent | null = null;
+
+function setMouseEventXY(evt: MouseEvent, x: number, y: number, isMove = false) {
+    evt.pageX = evt.offsetX = evt.screenX = evt.clientX = evt.x = x;
+    evt.pageY = evt.offsetY = evt.screenY = evt.clientY = evt.y = y;
+
+    evt.movementX = lastMoveMouseEvent ? evt.screenX - lastMoveMouseEvent.screenX : 0;
+    evt.movementY = lastMoveMouseEvent ? evt.screenY - lastMoveMouseEvent.screenY : 0;
+
+    if (isMove) {
+        lastMoveMouseEvent = evt;
+    }
+}
+
 let win: Window;
 let device: GPUDevice;
 let surface: Deno.UnsafeWindowSurface;
+let canvasDomMock: CanvasDomMock;
 
 export async function init(params: InitParams) {
     const adapter = await navigator.gpu.requestAdapter();
@@ -39,21 +126,7 @@ export async function init(params: InitParams) {
         height,
     })
 
-    const canvasDomMock = {
-        style: {},
-        // TODO: implement or polyfill the event system
-        addEventListener(event: string, ..._args: any[]) {
-            console.info(`canvas.addEventListener("${event}", ...)`)
-        },
-        getRootNode() {
-            console.info(`canvas.getRootNode()`);
-            return this;
-        },
-        getContext(name: string) {
-            console.info(`canvas.getContext("${name}")`);
-            return contextMock;
-        }
-    }
+    canvasDomMock = new CanvasDomMock(surface, width, height);
 
     const contextMock = {
         configure(configuration: GPUCanvasConfiguration) {
@@ -77,7 +150,6 @@ export async function init(params: InitParams) {
         device,
         context: contextMock,
         canvas: canvasDomMock,
-        surface,
     }
 }
 
@@ -91,6 +163,8 @@ let requestAnimationFrameCallbacks: FrameRequestCallback[] = [];
 
 const VALIDATION = Deno.args[0] == "--enable-validation";
 
+let button0 = 0;
+
 export async function runWindowEventLoop() {
     // TODO: Handle mouse and keyboard events, handle window resize event
     for await (const event of win.events()) {
@@ -100,6 +174,23 @@ export async function runWindowEventLoop() {
                 break;
             }
             continue;
+        } else if (event.type == EventType.MouseButtonDown) {
+            const evt = new MouseEvent("pointerdown");
+            setMouseEventXY(evt, event.x, event.y);
+            evt.buttons = 1;
+            button0 = 1;
+            canvasDomMock.dispatchEvent(evt);
+        } else if (event.type == EventType.MouseButtonUp) {
+            const evt = new MouseEvent("pointerup");
+            setMouseEventXY(evt, event.x, event.y);
+            evt.buttons = 0;
+            button0 = 0;
+            canvasDomMock.dispatchEvent(evt);
+        } else if (event.type == EventType.MouseMotion) {
+            const evt = new MouseEvent("pointermove");
+            setMouseEventXY(evt, event.x, event.y, true);
+            evt.buttons = button0;
+            canvasDomMock.dispatchEvent(evt);
         }
         // else if (event.type === EventType.WindowEvent) {
         //     switch (event.event) {
