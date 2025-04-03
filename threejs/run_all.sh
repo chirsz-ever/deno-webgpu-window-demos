cb8016e2e05e0b26602f9a8273139f63fdb4ecb4
chirsz-ever <chirsz-ever@outlook.com>
2025-04-04 01:38:05 +0800

    update threejs/run_all.sh

--- threejs/run_all.sh
+++ threejs/run_all.sh
@@ -3,20 +3,44 @@
 set -e
 THIS_DIR=$(dirname "$(realpath "$0")")
 
+failed_demos_output=threejs_failed_demos_$(uname -s)_$(uname -m).txt
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+    echo "Usage: $0 [demo_list_file]"
+    echo "If without arguments, it runs all WebGPU demos in $THIS_DIR."
+    echo "If with an argument, it runs the demos listed in the file."
+    echo "If any demo fails, it will be output to $failed_demos_output."
+    exit 0
+fi
+
+failed_demos_output=$PWD/$failed_demos_output
+
+if [[ -n $1 ]]; then
+    demo_list=()
+    while IFS='' read -r line; do demo_list+=("$line"); done < "$1"
+else
+    cd "$THIS_DIR"
+    demo_list=(webgpu_*.js)
+fi
+
 cd "$THIS_DIR"
 
-failed_items=()
+failed_demos=()
 
-for f in webgpu_*.js; do
-    echo -e "\033[34m[Run]\033[0m $f"
+i=0
+total=${#demo_list[@]}
+for f in "${demo_list[@]}"; do
+    i=$((i + 1))
+    echo -e "\033[34m[Run $i of $total]\033[0m $f"
     if ! deno run --allow-all "$f" --enable-validation; then
-        failed_items+=("$f")
+        failed_demos+=("$f")
     fi
 done
 
-if [ ${#failed_items[@]} -ne 0 ]; then
-    echo -e "\033[31mFailed demos:\033[0m"
-    for f in "${failed_items[@]}"; do
+if [ ${#failed_demos[@]} -ne 0 ]; then
+    for f in "${failed_demos[@]}"; do
         echo "$f"
-    done
+    done > "$failed_demos_output"
+    echo -e "\033[31m[Error]\033[0m Some demos failed. See $failed_demos_output for details."
+    exit 1
 fi
